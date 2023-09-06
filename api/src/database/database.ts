24f9405649f07c176f42e79ef2d18dd80ff1a4e4
Gustavo Cescon <gustavo.oliveira@cachoeiro.es.gov.br>
2023-09-06 14:16:54 -0300

    :tada: first commit

--- api/src/database/database.ts
+++ api/src/database/database.ts
@@ -3,12 +3,12 @@ import { Options } from "sequelize";
 const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT, DB_PORT } =
   process.env;
 const config: Options = {
-  username: "postgres",
-  password: "dtc@2016",
-  database: "bdCartaServico",
-  host: "10.1.1.70",
-  port: 5432,
-  dialect: "postgres",
+  username: "",
+  password: "",
+  database: "",
+  host: "",
+  port: 0,
+  dialect: "",
 };
 
 export = config;
